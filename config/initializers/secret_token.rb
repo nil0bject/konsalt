d4bc73bcaee5fb188f2dbd93de3e56e640b7c4a9
Nicholas Kirkwood <nick@gea-interactive.com.au>
2012-02-25 15:16:28 +1000

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Konsalt::Application.config.secret_token = 'a4bcbfa26776bd2624a4ab381a047e64a72070f2fd6e150a3cb0fe8edf09f5d5dc397167937decfb9c38c139a61806f5ad82d088f315d4e335010a9901444e13'
