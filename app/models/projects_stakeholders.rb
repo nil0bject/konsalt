099a0c81a80aab0e6974bc2ace3dfdcb08c5c572
nil0bject <nilobject@gmail.com>
2012-02-28 20:49:34 +1000

    created_at for projects_stakeholders insert(postgres)

--- app/models/projects_stakeholders.rb
+++ app/models/projects_stakeholders.rb
@@ -1,5 +1,11 @@
 class ProjectsStakeholders < ActiveRecord::Base
 belongs_to :project
 belongs_to :stakeholder
+
+before_create :set_creation_date
+def set_creation_date
+  params[:created_at] = DateTime.now
+end
+
 end
 
