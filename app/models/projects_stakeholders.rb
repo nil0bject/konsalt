44bceee0d99c0874dae9485ea261251f3cdf1add
nil0bject <nilobject@gmail.com>
2012-02-29 19:34:51 +1000

    ProjectsStakeholders timstamps before_validation_on_create

--- app/models/projects_stakeholders.rb
+++ app/models/projects_stakeholders.rb
@@ -2,9 +2,11 @@ class ProjectsStakeholders < ActiveRecord::Base
 belongs_to :project
 belongs_to :stakeholder
 
-before_create :set_creation_date
-def set_creation_date
-  params[:created_at] = DateTime.now
+before_create :set_timestamps
+def set_timestamps
+  puts "setting timestamps"
+  self[:created_at] = DateTime.now
+  self[:updated_at] = DateTime.now
 end
 
 end
